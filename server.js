5dd2a029c86b104b9f729acbf184746519f63de0
Chandu <43699825+chandan-reddy-k@users.noreply.github.com>
2020-01-30 21:40:38 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const next = require('next');
 const port = process.env.PORT || 3000;
+const dev = process.env.NODE_ENV !== 'production'
 const app = next({ dev });
 const handle = app.getRequestHandler();
 
